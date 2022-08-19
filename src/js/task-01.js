0fbc2e4513393ab3bedb1c9f58a98fdcf421f314
SPoluliakh <S_Poluliakh@ukr.net>
2022-08-19 23:56:09 +0300

    fixed 1 task

--- src/js/task-01.js
+++ src/js/task-01.js
@@ -1,6 +1,3 @@
-// const categoriesEl = document.querySelector('#categories');
-// console.log('Number of categories:', categoriesEl.children.length);
-
 const itemEl = document.querySelectorAll('#categories > .item');
 console.log(`Number of categories: ${itemEl.length}`);
 
@@ -10,12 +7,5 @@ function countItemElement() {
     console.log('Elements:', item.querySelectorAll('li').length);
   });
 }
-
-// function countItemElement() {
-//   itemEl.forEach(item => {
-//     console.log('Category:', item.firstElementChild.textContent);
-//     console.log('Elements:', item.lastElementChild.children.length);
-//   });
-// }
 
 countItemElement();
