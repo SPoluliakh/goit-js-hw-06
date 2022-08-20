8a66b4f694645b34e88f405869fadd4393c6c52f
SPoluliakh <S_Poluliakh@ukr.net>
2022-08-20 20:43:33 +0300

    9 done

--- src/js/task-09.js
+++ src/js/task-09.js
@@ -1,3 +1,14 @@
 function getRandomHexColor() {
   return `#${Math.floor(Math.random() * 16777215).toString(16)}`;
 }
+
+const btnEl = document.querySelector('.change-color');
+const colorValue = document.querySelector('.color');
+
+btnEl.addEventListener('click', onBtnClickChengeColor);
+
+function onBtnClickChengeColor() {
+  const color = getRandomHexColor();
+  document.body.style.backgroundColor = color;
+  colorValue.textContent = color;
+}
