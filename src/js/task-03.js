5bd9d07b49afc731d5713ffa0e3c2b92e27f9937
SPoluliakh <S_Poluliakh@ukr.net>
2022-08-20 23:22:31 +0300

    3 task - optimized

--- src/js/task-03.js
+++ src/js/task-03.js
@@ -16,9 +16,16 @@ const images = [
 const galleryEl = document.querySelector('.gallery');
 
 const makeGallery = ({ url, alt }) => {
-  return `<li class="gallery__item"><img class="gallery__img" src ='${url}' alt='${alt}'  /></li>`;
+  return `<li class="gallery__item"><img class="gallery__img" src ='${url}' alt='${alt}' width="400px" /></li>`;
 };
 
 const galleryItemEl = images.map(makeGallery).join('');
 
 galleryEl.insertAdjacentHTML('afterbegin', galleryItemEl);
+
+galleryEl.style.display = 'flex';
+galleryEl.style.displayWrap = 'wrap';
+galleryEl.style.listStyle = 'none';
+galleryEl.style.gap = '30px';
+galleryEl.style.justifyContent = 'center';
+galleryEl.style.alignItems = 'center';
