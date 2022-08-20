4c64d9178ceac973703913506d871c156303250a
SPoluliakh <S_Poluliakh@ukr.net>
2022-08-20 19:16:30 +0300

    8 done

--- src/js/task-08.js
+++ src/js/task-08.js
@@ -1,1 +1,23 @@
+const formEl = document.querySelector('.login-form');
 
+formEl.addEventListener('submit', onFormSubmit);
+
+function onFormSubmit(event) {
+  event.preventDefault();
+
+  const {
+    elements: { email, password },
+  } = event.currentTarget;
+
+  if (email.value === '' || password.value === '') {
+    return alert('please, fill in all the fields');
+  }
+
+  const fieledForm = {
+    email: email.value,
+    password: password.value,
+  };
+
+  console.log(fieledForm);
+  event.currentTarget.reset();
+}
