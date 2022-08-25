b5bc61ea1b06c81faf3f8198597a8c14b868326a
SPoluliakh <S_Poluliakh@ukr.net>
2022-08-25 22:31:09 +0300

    Updated task 5

--- src/js/task-05.js
+++ src/js/task-05.js
@@ -2,8 +2,6 @@ const inputEl = document.querySelector('#name-input');
 const outputNameEl = document.querySelector('#name-output');
 
 inputEl.addEventListener('input', event => {
-  outputNameEl.textContent = event.currentTarget.value;
-  if (event.currentTarget.value === '') {
-    outputNameEl.textContent = 'Anonymous';
-  }
+  outputNameEl.textContent =
+    event.currentTarget.value === '' ? 'Anonymous' : event.currentTarget.value;
 });
